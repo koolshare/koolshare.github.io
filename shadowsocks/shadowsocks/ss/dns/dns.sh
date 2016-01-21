6ec0e4e81c6daaceb638b42259ebfb592b9bb1ef
sadoneli <sadoneli@gmail.com>
2016-01-21 10:57:26 +0800

    fix ss to 0.9.6

--- shadowsocks/shadowsocks/ss/dns/dns.sh
+++ shadowsocks/shadowsocks/ss/dns/dns.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-
-source /koolshare/scripts/base.sh
 PROCS=Pcap_DNSProxy
-START=$PROCS
 CONFIG=/koolshare/ss/dns
 while true ; do
-sleep 18
 RUNNING=`ps|grep $PROCS|grep -v grep |wc -l`
-  if [ "${RUNNING}" -lt "1"  ];then
-  $START -c $CONFIG
-  echo "$(date) ${PROCS} is down,reboot!" >> /tmp/dns.log
+ if [ "${RUNNING}" -ge 1  ];then
+  sleep 1
+  exit 1
+  else
+  $PROCS -c $CONFIG
+  echo "$(date) Starting ${PROCS}!" >> /tmp/dns.log
+  sleep 1
   fi
 done
 exit 0
