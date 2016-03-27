3b3ae432badd4358d3a56602fe61ce2de0f9444f
janson <gandancing@gmail.com>
2016-03-27 12:20:36 +0800

    add config for aria2

---
+++ aria2/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+MODULE=aria2
+VERSION=1.5.6
+
+cat version
+rm -f ${MODULE}.tar.gz
+tar -zcvf ${MODULE}.tar.gz $MODULE
+md5value=`md5sum ${MODULE}.tar.gz|tr " " "\n"|sed -n 1p`
+cat > ./version <<EOF
+$VERSION
+$md5value
+EOF
+cat version
+
+cat > ./config.json.js <<EOF
+{
+"version':"$VERSION",
+"md5":"$md5value"
+}
+EOF
