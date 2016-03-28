0333b2f054130d159c5ac04b4e3300b28a2fef9e
王崇 <wangchong4@staff.weibo.com>
2016-03-28 13:47:29 +0800

    update speediest 0.2.0

--- speedtest/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cp -rf /tmp/speedtest/speedtest.sh /koolshare/scripts/
-cp -rf /tmp/speedtest/speedtest /koolshare/bin/
-cp -rf /tmp/speedtest/Module_speedtest.asp /koolshare/webs/
-rm -rf /tmp/speedtest* >/dev/null 2>&1
-
-chmod a+x /koolshare/scripts/speedtest.sh
-chmod a+x /koolshare/bin/speedtest
