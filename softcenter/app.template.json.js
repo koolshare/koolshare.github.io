f9deaa17f32439c40bf4ff3c7c66c75eb8a1ed36
janson <gandancing@gmail.com>
2016-04-06 22:56:35 +0800

    todo: auto generate app.json.js

---
+++ softcenter/app.template.json.js
@@ -0,0 +1,20 @@
+{
+	"curr_version":"1.0.5",
+	"curr_md5":"a16640f5a0df9186897be0de52e6629a",
+	"base_url":"https://raw.githubusercontent.com/koolshare/koolshare.github.io/master",
+
+	"apps":
+[
+{
+	"name":"adm",
+	"title":"阿呆猫",
+	"ver":"0.5",
+	"home_url":"/Module_xunlei.asp",
+	"tar_url":"adm/adm.tar.gz",
+	"md5":"31559b347f6c8081ccdece78e50156d5",
+},
+{
+}
+]
+
+}
