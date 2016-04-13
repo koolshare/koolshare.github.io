b857542c76c07959990e1e6a506981cea204a06c
Nemo <zhhjchina@gmail.com>
2016-04-13 22:17:24 +0800

    Revert "Fix build folder"
    This reverts commit 6d19666d8b85ebc8b1b3eee42ebceda05978151b.

--- aria2/build.sh
+++ aria2/build.sh
@@ -6,7 +6,7 @@ VERSION=`cat aria2/aria2/version`
 cat version
 rsync -av --include="aria2/www/glutton/dist" --include="aria2/www/glutton/index.html" --exclude="aria2/www/glutton/*" aria2 build_dir/
 rm -f ${MODULE}.tar.gz
-tar -zcvf ${MODULE}.tar.gz -C build_dir/ $MODULE 
+tar -zcvf ${MODULE}.tar.gz build_dir/$MODULE
 md5value=`md5sum ${MODULE}.tar.gz|tr " " "\n"|sed -n 1p`
 cat > ./version <<EOF
 $VERSION
