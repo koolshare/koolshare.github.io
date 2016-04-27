05c884b1d99e9127b64bfe3610c84b373c28aa99
sadoneli <sadoneli@gmail.com>
2016-04-28 01:08:39 +0800

    kuainiao for softcenter

--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -56,6 +56,11 @@
             "version": "1.4"
         }, 
         {
+            "name": "v2ray", 
+            "tar_url": "v2ray/v2ray.tar.gz", 
+            "title": "v2ray"
+        }, 
+        {
             "build_date": "2016-04-27_11:22:01", 
             "description": "点对点建立连接,下载更快", 
             "home_url": "", 
@@ -66,17 +71,17 @@
             "version": "0.2"
         }, 
         {
-            "build_date": "2016-04-27_10:35:26", 
+            "build_date": "2016-04-28_01:07:53", 
             "description": "迅雷快鸟，不解释~", 
             "home_url": "", 
-            "md5": "e078ba7906fcb783e5862a75d573fcaa", 
+            "md5": "4e63031b121655cd9b7e8f1f07bd27bd", 
             "name": "kuainiao", 
             "tar_url": "kuainiao/kuainiao.tar.gz", 
             "title": "讯雷快鸟", 
             "version": "0.1.0"
         }, 
         {
-            "description": "aria2", 
+            "description": "迅雷不好,试试这个", 
             "home_url": "", 
             "md5": "167ab07a513df44612bd0140084829d3", 
             "name": "aria2", 
