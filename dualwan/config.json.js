91e0d59c64ddadaa1d92f0bd25fd82f661704364
janson <gandancing@gmail.com>
2016-04-28 16:48:04 +0800

    dualwan support

---
+++ dualwan/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"0.2",
+"md5":"b18840be1d13defa25a29f85bb5fa269",
+"home_url":"Module_policy_route.asp",
+"title":"策略路由",
+"description":"让分流更简单",
+"build_date":"2016-04-28_16:47:23"
+}
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -1,46 +1,6 @@
 {
     "apps": [
         {
-            "build_date": "2016-04-27_23:31:52", 
-            "description": "让测速更简单", 
-            "home_url": "Module_speedtest.asp", 
-            "md5": "5a072a57af3b9bb1343a241a44278dd2", 
-            "name": "speedtest", 
-            "tar_url": "speedtest/speedtest.tar.gz", 
-            "title": "网络测速", 
-            "version": "0.2.2"
-        }, 
-        {
-            "build_date": "2016-04-27_12:07:46", 
-            "description": "", 
-            "home_url": "Module_tunnel.asp", 
-            "md5": "d747425ac60b7c2a614b83c39fddd641", 
-            "name": "tunnel", 
-            "tar_url": "tunnel/tunnel.tar.gz", 
-            "title": "穿透DDNS", 
-            "version": "0.2"
-        }, 
-        {
-            "build_date": "2016-04-28_01:34:06", 
-            "description": "SVPN让游戏更畅快", 
-            "home_url": "Module_shadowvpn.asp", 
-            "md5": "68accc73b17e5084580690c53900f67f", 
-            "name": "shadowvpn", 
-            "tar_url": "shadowvpn/shadowvpn.tar.gz", 
-            "title": "Shadowvpn", 
-            "version": "2.3"
-        }, 
-        {
-            "build_date": "2016-04-27_19:43:43", 
-            "description": "ss-server", 
-            "home_url": "Module_ssserver.asp", 
-            "md5": "eb21f53915f8fae3502b32c94ce3ff9a", 
-            "name": "ssserver", 
-            "tar_url": "ssserver/ssserver.tar.gz", 
-            "title": "ss-server", 
-            "version": "1.2"
-        }, 
-        {
             "build_date": "2016-04-27_20:51:46", 
             "description": "去广告，看疗效~", 
             "home_url": "Module_adm.asp", 
@@ -51,23 +11,13 @@
             "version": "1.3"
         }, 
         {
-            "build_date": "2016-04-27_22:34:37", 
-            "description": "迅雷远程~", 
-            "home_url": "Module_thunder.asp", 
-            "md5": "a19611c2e00f2f1269e15c03e647d6b0", 
-            "name": "thunder", 
-            "tar_url": "thunder/thunder.tar.gz", 
-            "title": "迅雷远程", 
-            "version": "1.4"
-        }, 
-        {
-            "build_date": "2016-04-27_11:22:01", 
-            "description": "点对点建立连接,下载更快", 
-            "home_url": "", 
-            "md5": "0bdc9228d8f3d9f46f7bad25bf47a04a", 
-            "name": "koolnet", 
-            "tar_url": "koolnet/koolnet.tar.gz", 
-            "title": "P2P 穿透", 
+            "build_date": "2016-04-27_12:07:46", 
+            "description": "", 
+            "home_url": "Module_tunnel.asp", 
+            "md5": "d747425ac60b7c2a614b83c39fddd641", 
+            "name": "tunnel", 
+            "tar_url": "tunnel/tunnel.tar.gz", 
+            "title": "穿透DDNS", 
             "version": "0.2"
         }, 
         {
@@ -81,6 +31,56 @@
             "version": "0.1.1"
         }, 
         {
+            "build_date": "2016-04-27_19:43:43", 
+            "description": "ss-server", 
+            "home_url": "Module_ssserver.asp", 
+            "md5": "eb21f53915f8fae3502b32c94ce3ff9a", 
+            "name": "ssserver", 
+            "tar_url": "ssserver/ssserver.tar.gz", 
+            "title": "ss-server", 
+            "version": "1.2"
+        }, 
+        {
+            "build_date": "2016-04-27_22:34:37", 
+            "description": "迅雷远程~", 
+            "home_url": "Module_thunder.asp", 
+            "md5": "a19611c2e00f2f1269e15c03e647d6b0", 
+            "name": "thunder", 
+            "tar_url": "thunder/thunder.tar.gz", 
+            "title": "迅雷远程", 
+            "version": "1.4"
+        }, 
+        {
+            "build_date": "2016-04-28_01:34:06", 
+            "description": "SVPN让游戏更畅快", 
+            "home_url": "Module_shadowvpn.asp", 
+            "md5": "68accc73b17e5084580690c53900f67f", 
+            "name": "shadowvpn", 
+            "tar_url": "shadowvpn/shadowvpn.tar.gz", 
+            "title": "Shadowvpn", 
+            "version": "2.3"
+        }, 
+        {
+            "build_date": "2016-04-27_23:31:52", 
+            "description": "让测速更简单", 
+            "home_url": "Module_speedtest.asp", 
+            "md5": "5a072a57af3b9bb1343a241a44278dd2", 
+            "name": "speedtest", 
+            "tar_url": "speedtest/speedtest.tar.gz", 
+            "title": "网络测速", 
+            "version": "0.2.2"
+        }, 
+        {
+            "build_date": "2016-04-28_16:47:23", 
+            "description": "让分流更简单", 
+            "home_url": "Module_policy_route.asp", 
+            "md5": "b18840be1d13defa25a29f85bb5fa269", 
+            "name": "dualwan", 
+            "tar_url": "dualwan/dualwan.tar.gz", 
+            "title": "策略路由", 
+            "version": "0.2"
+        }, 
+        {
             "description": "迅雷不好,试试这个", 
             "home_url": "", 
             "md5": "167ab07a513df44612bd0140084829d3", 
@@ -88,10 +88,20 @@
             "tar_url": "aria2/aria2.tar.gz", 
             "title": "", 
             "version": "1.5.8"
+        }, 
+        {
+            "build_date": "2016-04-27_11:22:01", 
+            "description": "点对点建立连接,下载更快", 
+            "home_url": "", 
+            "md5": "0bdc9228d8f3d9f46f7bad25bf47a04a", 
+            "name": "koolnet", 
+            "tar_url": "koolnet/koolnet.tar.gz", 
+            "title": "P2P 穿透", 
+            "version": "0.2"
         }
     ], 
     "home_url": "https://raw.githubusercontent.com/koolshare/koolshare.github.io/master", 
-    "md5": "46805b4e09a57673086d85bf1ea5e172", 
+    "md5": "bf9915619802e4b99d90291d623142f4", 
     "tar_url": "softcenter/softcenter.tar.gz", 
     "version": "1.0.5"
 }
