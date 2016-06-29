a344231b298222b82977d8ddaa8014c8ae3165a2
janson <gandancing@gmail.com>
2016-06-29 11:51:21 +0800

    phddns fix

--- phddns/config.json.js
+++ phddns/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"0.2",
-"md5":"d4d5d02bf6911675857eef574bcfe466",
+"md5":"3631291ffa400f09c57b0b8a789e1161",
 "home_url":"",
 "title":"花生壳内网版",
 "description":"让局域网控制能简单",
-"build_date":"2016-06-28_15:17:04"
+"build_date":"2016-06-29_11:33:47"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -121,10 +121,10 @@
             "version": "0.2"
         }, 
         {
-            "build_date": "2016-06-28_15:17:04", 
+            "build_date": "2016-06-29_11:33:47", 
             "description": "让局域网控制能简单", 
             "home_url": "", 
-            "md5": "d4d5d02bf6911675857eef574bcfe466", 
+            "md5": "3631291ffa400f09c57b0b8a789e1161", 
             "name": "phddns", 
             "tar_url": "phddns/phddns.tar.gz", 
             "title": "花生壳内网版", 
