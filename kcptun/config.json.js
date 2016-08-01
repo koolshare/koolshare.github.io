3ec7ce54c4b437a3569cf2644a163b2c2f965967
sadoneli <sadoneli@gmail.com>
2016-08-01 09:39:17 +0800

    kcptun test

---
+++ kcptun/config.json.js
@@ -0,0 +1,8 @@
+{
+"version":"1.0",
+"md5":"77c6c55a6480df8b542f2acef981ac0a",
+"home_url":"Module_kcp.asp",
+"title":"kcptun",
+"description":"An extremely simple & fast udp tunnel based on kcp protocol",
+"build_date":"2016-07-31_09:11:08"
+}
