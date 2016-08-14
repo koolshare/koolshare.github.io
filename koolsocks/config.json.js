43fc93b5c0c881ef682a79c25c1b481f2c613d64
sadoneli <sadoneli@gmail.com>
2016-08-15 02:09:20 +0800

    not finished yet

--- koolsocks/config.json.js
+++ koolsocks/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"1.5.6",
-"md5":"cff5f49562ca3e6f369b60424ccd9262",
+"md5":"f1acead121d528dd799c383524c266dd",
 "home_url":"Main_Ss_Content.asp",
 "title":"shadowsocks",
 "description":"科学上网",
-"build_date":"2016-08-15_01:48:33"
+"build_date":"2016-08-15_02:08:57"
 }
--- koolsocks/koolsocks/install .sh
+++ koolsocks/koolsocks/install .sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 cd /tmp
-cp -rf /tmp/shadowsocks/ss/* /koolshare/ss/
-cp -rf /tmp/shadowsocks/webs/* /koolshare/webs/
-cp -rf /tmp/shadowsocks/res/* /koolshare/res/
-cp -rf /tmp/shadowsocks/scripts/* /koolshare/scripts/
-cp -rf /tmp/shadowsocks/bin/* /koolshare/bin/
-cp -rf /tmp/shadowsocks/init.d/* /koolshare/init.d/
-rm -rf /tmp/shadowsocks* >/dev/null 2>&1
+cp -rf /tmp/koolsocks/ss/* /koolshare/ss/
+cp -rf /tmp/koolsocks/webs/* /koolshare/webs/
+cp -rf /tmp/koolsocks/res/* /koolshare/res/
+cp -rf /tmp/koolsocks/scripts/* /koolshare/scripts/
+cp -rf /tmp/koolsocks/bin/* /koolshare/bin/
+cp -rf /tmp/koolsocks/init.d/* /koolshare/init.d/
+rm -rf /tmp/koolsocks* >/dev/null 2>&1
 
 # no use since version 1.0.0
 rm -rf /koolshare/ss/ssconfig
@@ -24,4 +24,6 @@ chmod 755 /koolshare/ss/socks5/*
 chmod 755 /koolshare/ss/*.sh
 chmod 755 /koolshare/scripts/*
 chmod 755 /koolshare/bin/*
+
+rm -rf /tmp/koolsocks*
 
