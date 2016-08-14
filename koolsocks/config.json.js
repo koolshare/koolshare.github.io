d1ed2f645272076bc0c460dde7636465bb554b02
sadoneli <sadoneli@gmail.com>
2016-08-15 01:49:09 +0800

    fix icon name

--- koolsocks/config.json.js
+++ koolsocks/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"1.5.6",
-"md5":"3a2ec8dab4b59af07cc09924642e540f",
+"md5":"cff5f49562ca3e6f369b60424ccd9262",
 "home_url":"Main_Ss_Content.asp",
 "title":"shadowsocks",
 "description":"科学上网",
-"build_date":"2016-08-15_00:57:16"
+"build_date":"2016-08-15_01:48:33"
 }
