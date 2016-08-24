ac50a41691c707b34a2cad55c9b28ad6c63788f7
sadoneli <sadoneli@gmail.com>
2016-08-25 01:12:07 +0800

    remove koolsocks for it default installed in software center

--- koolsocks/config.json.js
+++
@@ -1,8 +0,0 @@
-{
-"version":"1.3",
-"md5":"a7c700a71213de3f46018eddc99aa8eb",
-"home_url":"Main_Ss_Content.asp",
-"title":"shadowsocks",
-"description":"科学上网",
-"build_date":"2016-08-21_23:09:57"
-}
--- koolsocks/koolsocks/install.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-cd /tmp
-
-cp -rf /tmp/koolsocks/res/* /koolshare/res/
-
-rm -rf /tmp/koolsocks* >/dev/null 2>&1
-
