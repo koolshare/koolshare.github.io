2ad49315ecdb8bd0c801cfb404f0f8501a612bc6
sadoneli <sadoneli@gmail.com>
2016-09-15 13:55:36 +0800

    kp1.6

--- koolproxy/build.sh
+++ koolproxy/build.sh
@@ -2,7 +2,7 @@
 
 
 MODULE=koolproxy
-VERSION=1.5
+VERSION=1.6
 TITLE=koolproxy
 DESCRIPTION=koolproxy，支持正则的网络代理~
 HOME_URL=Module_koolproxy.asp
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -1,53 +1,23 @@
 {
     "apps": [
         {
-            "build_date": "2016-08-08_21:12:31", 
-            "description": "ss-server", 
-            "home_url": "Module_ssserver.asp", 
-            "md5": "8410c415cd52dcf229ed2d1625c02434", 
-            "name": "ssserver", 
-            "tar_url": "ssserver/ssserver.tar.gz", 
-            "title": "ss-server", 
-            "version": "1.3"
+            "build_date": "2016-04-27_23:31:52", 
+            "description": "让测速更简单", 
+            "home_url": "Module_speedtest.asp", 
+            "md5": "5a072a57af3b9bb1343a241a44278dd2", 
+            "name": "speedtest", 
+            "tar_url": "speedtest/speedtest.tar.gz", 
+            "title": "网络测速", 
+            "version": "0.2.2"
         }, 
         {
-            "build_date": "2016-06-20_17:00:29", 
-            "description": "迅雷快鸟，为上网加速而生~", 
-            "home_url": "Module_kuainiao.asp", 
-            "md5": "9bb032598894d2a7f4f7356127e725a9", 
-            "name": "kuainiao", 
-            "tar_url": "kuainiao/kuainiao.tar.gz", 
-            "title": "讯雷快鸟", 
-            "version": "0.3.6"
-        }, 
-        {
-            "build_date": "2016-09-15_12:16:25", 
-            "description": "koolproxy，支持正则的网络代理~", 
-            "home_url": "Module_koolproxy.asp", 
-            "md5": "e7b7af3a7da9c65471f737a50966e3f7", 
-            "name": "koolproxy", 
-            "tar_url": "koolproxy/koolproxy.tar.gz", 
-            "title": "koolproxy", 
-            "version": "1.5"
-        }, 
-        {
-            "build_date": "2016-09-05_18:40:27", 
-            "description": "linux下载利器", 
-            "home_url": "Module_aria2.asp", 
-            "md5": "c4a4e1e8b7fdfe588f75ef4defb15a23", 
-            "name": "aria2", 
-            "tar_url": "aria2/aria2.tar.gz", 
-            "title": "Aria2", 
-            "version": "1.7.4"
-        }, 
-        {
-            "build_date": "2016-05-07_01:41:30", 
-            "description": "点对点建立连接,下载更快", 
-            "home_url": "", 
-            "md5": "21f65a490064f311481d3db3c5068a9d", 
-            "name": "koolnet", 
-            "tar_url": "koolnet/koolnet.tar.gz", 
-            "title": "P2P 穿透", 
+            "build_date": "2016-05-07_01:56:11", 
+            "description": "穿透 DDNS, 服务器转发方式", 
+            "home_url": "Module_tunnel.asp", 
+            "md5": "4fb086ad875d5602074bc0eab876b7eb", 
+            "name": "tunnel", 
+            "tar_url": "tunnel/tunnel.tar.gz", 
+            "title": "穿透DDNS", 
             "version": "0.2"
         }, 
         {
@@ -61,33 +31,13 @@
             "version": "2.7"
         }, 
         {
-            "build_date": "2016-09-09_16:12:08", 
-            "description": "基于kcp协议的udp网络加速", 
-            "home_url": "Module_kcptun.asp", 
-            "md5": "1bf62a5bc715f2276ea25da19ccc7971", 
-            "name": "kcptun", 
-            "tar_url": "kcptun/kcptun.tar.gz", 
-            "title": "kcptun", 
-            "version": "1.9"
-        }, 
-        {
-            "build_date": "2016-05-13_16:14:27", 
-            "description": "加速apple服务", 
-            "home_url": "Module_appledns.asp", 
-            "md5": "b36145d26b9a896a329c8facd9020466", 
-            "name": "appledns", 
-            "tar_url": "appledns/appledns.tar.gz", 
-            "title": "AppleDNS", 
-            "version": "0.2"
-        }, 
-        {
-            "build_date": "2016-05-19_22:37:04", 
-            "description": "中文SSID，装逼利器！", 
-            "home_url": "Module_ssid.asp", 
-            "md5": "fe117409dde27578229650f93465efaf", 
-            "name": "ssid", 
-            "tar_url": "ssid/ssid.tar.gz", 
-            "title": "中文SSID", 
+            "build_date": "2016-08-08_21:12:31", 
+            "description": "ss-server", 
+            "home_url": "Module_ssserver.asp", 
+            "md5": "8410c415cd52dcf229ed2d1625c02434", 
+            "name": "ssserver", 
+            "tar_url": "ssserver/ssserver.tar.gz", 
+            "title": "ss-server", 
             "version": "1.3"
         }, 
         {
@@ -101,16 +51,6 @@
             "version": "1.5"
         }, 
         {
-            "build_date": "2016-05-11_19:20:01", 
-            "description": "让分流更简单", 
-            "home_url": "Module_policy_route.asp", 
-            "md5": "38cd706eb85c05dd4da74b4490694473", 
-            "name": "dualwan", 
-            "tar_url": "dualwan/dualwan.tar.gz", 
-            "title": "策略路由", 
-            "version": "0.6"
-        }, 
-        {
             "build_date": "2016-05-10_21:35:41", 
             "description": "老板，来一斤虚拟内存~", 
             "home_url": "Module_swap.asp", 
@@ -121,34 +61,14 @@
             "version": "1.8"
         }, 
         {
-            "build_date": "2016-06-29_11:33:47", 
-            "description": "让局域网控制能简单", 
-            "home_url": "", 
-            "md5": "3631291ffa400f09c57b0b8a789e1161", 
-            "name": "phddns", 
-            "tar_url": "phddns/phddns.tar.gz", 
-            "title": "花生壳内网版", 
-            "version": "0.2"
-        }, 
-        {
-            "build_date": "2016-09-11_11:42:19", 
-            "description": "使用Dnspod的ddns服务", 
-            "home_url": "Module_ddnspod.asp", 
-            "md5": "232e4bfc7fd823f267dc8a1d1721fb46", 
-            "name": "ddnspod", 
-            "tar_url": "ddnspod/ddnspod.tar.gz", 
-            "title": "DDnspod", 
-            "version": "0.1.4"
-        }, 
-        {
-            "build_date": "2016-05-07_01:56:11", 
-            "description": "穿透 DDNS, 服务器转发方式", 
-            "home_url": "Module_tunnel.asp", 
-            "md5": "4fb086ad875d5602074bc0eab876b7eb", 
-            "name": "tunnel", 
-            "tar_url": "tunnel/tunnel.tar.gz", 
-            "title": "穿透DDNS", 
-            "version": "0.2"
+            "build_date": "2016-09-09_16:12:08", 
+            "description": "基于kcp协议的udp网络加速", 
+            "home_url": "Module_kcptun.asp", 
+            "md5": "1bf62a5bc715f2276ea25da19ccc7971", 
+            "name": "kcptun", 
+            "tar_url": "kcptun/kcptun.tar.gz", 
+            "title": "kcptun", 
+            "version": "1.9"
         }, 
         {
             "build_date": "2016-05-09_23:58:50", 
@@ -161,14 +81,94 @@
             "version": "1.6"
         }, 
         {
-            "build_date": "2016-04-27_23:31:52", 
-            "description": "让测速更简单", 
-            "home_url": "Module_speedtest.asp", 
-            "md5": "5a072a57af3b9bb1343a241a44278dd2", 
-            "name": "speedtest", 
-            "tar_url": "speedtest/speedtest.tar.gz", 
-            "title": "网络测速", 
-            "version": "0.2.2"
+            "build_date": "2016-05-07_01:41:30", 
+            "description": "点对点建立连接,下载更快", 
+            "home_url": "", 
+            "md5": "21f65a490064f311481d3db3c5068a9d", 
+            "name": "koolnet", 
+            "tar_url": "koolnet/koolnet.tar.gz", 
+            "title": "P2P 穿透", 
+            "version": "0.2"
+        }, 
+        {
+            "build_date": "2016-09-15_13:55:23", 
+            "description": "koolproxy，支持正则的网络代理~", 
+            "home_url": "Module_koolproxy.asp", 
+            "md5": "a306ef13cc445094e3c936f9d31d4190", 
+            "name": "koolproxy", 
+            "tar_url": "koolproxy/koolproxy.tar.gz", 
+            "title": "koolproxy", 
+            "version": "1.6"
+        }, 
+        {
+            "build_date": "2016-05-19_22:37:04", 
+            "description": "中文SSID，装逼利器！", 
+            "home_url": "Module_ssid.asp", 
+            "md5": "fe117409dde27578229650f93465efaf", 
+            "name": "ssid", 
+            "tar_url": "ssid/ssid.tar.gz", 
+            "title": "中文SSID", 
+            "version": "1.3"
+        }, 
+        {
+            "build_date": "2016-06-20_17:00:29", 
+            "description": "迅雷快鸟，为上网加速而生~", 
+            "home_url": "Module_kuainiao.asp", 
+            "md5": "9bb032598894d2a7f4f7356127e725a9", 
+            "name": "kuainiao", 
+            "tar_url": "kuainiao/kuainiao.tar.gz", 
+            "title": "讯雷快鸟", 
+            "version": "0.3.6"
+        }, 
+        {
+            "build_date": "2016-06-29_11:33:47", 
+            "description": "让局域网控制能简单", 
+            "home_url": "", 
+            "md5": "3631291ffa400f09c57b0b8a789e1161", 
+            "name": "phddns", 
+            "tar_url": "phddns/phddns.tar.gz", 
+            "title": "花生壳内网版", 
+            "version": "0.2"
+        }, 
+        {
+            "build_date": "2016-09-05_18:40:27", 
+            "description": "linux下载利器", 
+            "home_url": "Module_aria2.asp", 
+            "md5": "c4a4e1e8b7fdfe588f75ef4defb15a23", 
+            "name": "aria2", 
+            "tar_url": "aria2/aria2.tar.gz", 
+            "title": "Aria2", 
+            "version": "1.7.4"
+        }, 
+        {
+            "build_date": "2016-09-11_11:42:19", 
+            "description": "使用Dnspod的ddns服务", 
+            "home_url": "Module_ddnspod.asp", 
+            "md5": "232e4bfc7fd823f267dc8a1d1721fb46", 
+            "name": "ddnspod", 
+            "tar_url": "ddnspod/ddnspod.tar.gz", 
+            "title": "DDnspod", 
+            "version": "0.1.4"
+        }, 
+        {
+            "build_date": "2016-05-11_19:20:01", 
+            "description": "让分流更简单", 
+            "home_url": "Module_policy_route.asp", 
+            "md5": "38cd706eb85c05dd4da74b4490694473", 
+            "name": "dualwan", 
+            "tar_url": "dualwan/dualwan.tar.gz", 
+            "title": "策略路由", 
+            "version": "0.6"
+        }, 
+        {
+            "build_date": "2016-05-13_16:14:27", 
+            "description": "加速apple服务", 
+            "home_url": "Module_appledns.asp", 
+            "md5": "b36145d26b9a896a329c8facd9020466", 
+            "name": "appledns", 
+            "tar_url": "appledns/appledns.tar.gz", 
+            "title": "AppleDNS", 
+            "version": "0.2"
         }
     ], 
     "home_url": "https://raw.githubusercontent.com/koolshare/koolshare.github.io/master", 
