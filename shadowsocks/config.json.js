0bdb638fa6348194fd16621e4cdce850b1fc5b1c
sadoneli <sadoneli@gmail.com>
2016-10-08 02:35:22 +0800

    ss 295

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"2.9.4",
-"md5":"d7909384f6ee8d3982983113e4761962",
+"version":"2.9.5",
+"md5":"0c314f12aca3c5df4d0618872725afa5",
 "home_url":"Main_Ss_Content.asp",
 "title":"shadowsocks",
 "description":"科学上网",
-"build_date":"2016-10-06_21:57:53"
+"build_date":"2016-10-08_02:35:14"
 }
