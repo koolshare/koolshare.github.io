9abf303bd57918e6eeea36b9774303c6bcc92288
Major <Major-k@users.noreply.github.com>
2016-10-27 19:25:39 +0800

    Update install.sh

--- kms/kms/install.sh
+++ kms/kms/install.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-dbus set kms_version="0.1"
-dbus set softcenter_module_kms_version="0.1"
-dbus set softcenter_module_kms_description="Office自动激活工具"
 
 cp -rf /tmp/kms/scripts/* /koolshare/scripts/
 cp -rf /tmp/kms/bin/* /koolshare/bin/
