b358a75027ba0312dd53e974a198c2a4ffdbf194
sadoneli <sadoneli@gmail.com>
2016-10-27 21:55:23 +0800

    koolproxy 2.0.6#3

--- koolproxy/config.json.js
+++ koolproxy/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"2.0.6",
-"md5":"eddc75a7daf995e95812bb85f9ec5080",
+"md5":"1112c457845fdcbf466aefd291f07896",
 "home_url":"Module_koolproxy.asp",
 "title":"koolproxy",
 "description":"koolproxy，支持正则的网络代理~",
-"build_date":"2016-10-27_21:48:34"
+"build_date":"2016-10-27_21:55:06"
 }
