00ad42a7ea6a17b8797391962c4a7c66a92ea30f
Major <Major-k@users.noreply.github.com>
2016-10-27 09:08:10 +0800

    Update config.json.js

--- kms/config.json.js
+++ kms/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"0.1",
-"md5":"73AE5192EFD462D1EB1AEB853D49D722",
+"version":"0.2",
+"md5":"DE07A658EFFDE0E04E763784C95386A0",
 "home_url":"Module_kms.asp",
 "title":"kms",
 "description":"Office自动激活工具",
