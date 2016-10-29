e7b53fe776cbc208cce27148657ee61be6a37045
Major <Major-k@users.noreply.github.com>
2016-10-29 16:42:21 +0800

    Update config.json.js

--- kms/config.json.js
+++ kms/config.json.js
@@ -1,6 +1,6 @@
 {
-"version":"0.8",
-"md5":"9fff310e402a93734cd15ed6c266aa1b",
+"version":"0.9",
+"md5":"69cfb5c4b38c62fd622a63785fb63b7b",
 "home_url":"Module_kms.asp",
 "title":"系统工具",
 "description":"来自网络的胃軟系统工具",
