00439098342590f793049638427372a3c74c1581
sadoneli <sadoneli@gmail.com>
2016-11-06 19:16:35 +0800

    koolproxy 2.0.8#3

--- koolproxy/config.json.js
+++ koolproxy/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"2.0.8",
-"md5":"e81d89d956e37e08e3c32a722bfc2669",
+"md5":"d64f5d151a80ce3e08e2ce20d41df8c3",
 "home_url":"Module_koolproxy.asp",
 "title":"koolproxy",
 "description":"koolproxy，支持正则的网络代理~",
-"build_date":"2016-11-06_19:09:38"
+"build_date":"2016-11-06_19:16:31"
 }
--- softcenter/app.json.js
+++ softcenter/app.json.js
@@ -131,10 +131,10 @@
             "version": "0.9"
         }, 
         {
-            "build_date": "2016-11-06_19:09:38", 
+            "build_date": "2016-11-06_19:16:31", 
             "description": "koolproxy，支持正则的网络代理~", 
             "home_url": "Module_koolproxy.asp", 
-            "md5": "e81d89d956e37e08e3c32a722bfc2669", 
+            "md5": "d64f5d151a80ce3e08e2ce20d41df8c3", 
             "name": "koolproxy", 
             "tar_url": "koolproxy/koolproxy.tar.gz", 
             "title": "koolproxy", 
