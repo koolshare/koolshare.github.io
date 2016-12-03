7728a3894ef510f1e29629f2a86f827afaeef019
lixin <sadoneli@gmail.com>
2016-12-03 11:50:51 +0800

    detach aria2

--- aria2/aria2/scripts/aria2_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-eval `dbus export aria2`
-
-if [ $aria2_enable = 1 ];then
-	sh /koolshare/aria2/aria2_run.sh restart
-else
-	sh /koolshare/aria2/aria2_run.sh stop
-fi
--- aria2/config.json.js
+++
@@ -1,8 +0,0 @@
-{
-"version":"1.7.5",
-"md5":"bdb818d74c1800ff90309d0282410ad3",
-"home_url":"Module_aria2.asp",
-"title":"Aria2",
-"description":"linux下载利器",
-"build_date":"2016-11-23_16:12:15"
-}
