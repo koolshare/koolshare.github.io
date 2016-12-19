f7e9b5ab6ee8a64742cb6f3a67c455c72408e40a
clang <clang@debian>
2016-12-19 16:40:25 +0800

    frpc 1.9.3

--- frpc/config.json.js
+++
@@ -1,8 +0,0 @@
-{
-"version":"1.9.2",
-"md5":"baca9f9b933ceb3e9f8546507e3d7b15",
-"home_url":"Module_frpc.asp",
-"title":"frpc穿透",
-"description":"内网穿透利器，谁用谁知道。",
-"build_date":"2016-12-03_17:59:45"
-}
