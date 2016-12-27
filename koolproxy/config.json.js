df5d48c3a83a8c1c97e97b19c7e476abeb60425c
lixin <sadoneli@gmail.com>
2016-12-27 10:34:36 +0800

    detach koolproxy

--- koolproxy/config.json.js
+++
@@ -1,8 +0,0 @@
-{
-"version":"3.0.0",
-"md5":"464f960699e8a54c78ecfb85e447419c",
-"home_url":"Module_koolproxy.asp",
-"title":"koolproxy",
-"description":"3.0.0版本代码重构,性能更强,拒绝bug~",
-"build_date":"2016-12-27_09:15:58"
-}
--- koolproxy/koolproxy/scripts/koolproxy_config.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-eval `dbus export koolproxy`
-
-
-if [ "$koolproxy_enable" == "1" ];then
-	sh /koolshare/koolproxy/koolproxy.sh restart
-else
-	sh /koolshare/koolproxy/koolproxy.sh stop
-fi
