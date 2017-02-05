f126283f7f2a7dfc0c35fa222cda943d9858fc9b
sadoneli <sadoneli@gmail.com>
2017-02-06 02:48:40 +0800

    Update push_message.json.js

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,6 +1,6 @@
 {
 "title":"<em>梅林改版固件&nbsp;-&nbsp;软件中心</em>",
-"content1":"<b><font size="4px">你可能用了假的koolproxy！</font></b>",
+"content1":"<b><font size='4px'>你可能用了假的koolproxy！</font></b>",
 "content2":"因为我个人（sadog）的误操作，导致ss安装包里带了3.1.0-beta5的koolproxy程序，而更新ss插件会导致koolproxy程序会被这个老的koolproxy覆盖！",
 "content3":"这里建议大家更新koolproxy 3.2.5.1，并检查插件版本和运行状态里的版本是否一致，如果不一致，建议大家关闭重装koolproxy！",
 "content4":"这里对大家表示非常的抱歉！"
