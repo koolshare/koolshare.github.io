22daab4d00624020adbe6ff9dbad21ef699dbd5a
sadoneli <sadoneli@gmail.com>
2017-02-06 02:38:41 +0800

    Update push_message.json.js

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>梅林改版固件&nbsp;-&nbsp;软件中心</em>",
-"content1":"软件中心更新通知：",
-"content2":"为了兼容新固件380.65-X7.4固件web菜单，软件中心本身和其中的很多软件，在固件发布之前提前更新一轮",
-"content3":"X7.4版本以下的固件不会受到兼容代码的影响，请大家放心！",
-"content4":"需要帮助？请到telegram群：https://t.me/joinchat/AAAAAEC7pgV9vPdPcJ4dJw"
+"content1":"<b>你可能用了假的koolproxy！</b>",
+"content2":"因为我个人（sadog）的误操作，导致ss安装包里带了3.1.0-beta5的koolproxy程序，而更新ss插件会导致koolproxy程序会被这个老的koolproxy覆盖！",
+"content3":"这里建议大家更新koolproxy 3.2.5.1，并检查插件版本和运行状态里的版本是否一致，如果不一致，建议大家关闭重装koolproxy！",
+"content4":"这里对大家表示非常的抱歉！"
 }
