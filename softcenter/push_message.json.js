8ff43fa5caa2185114fde2a2fb714701375b7c18
lixin <sadoneli@gmail.com>
2017-06-10 00:48:27 +0800

    update message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>梅林改版固件&nbsp;-&nbsp;软件中心&nbsp;-&nbsp;Mipsel</em>",
 "content1":"通知：",
-"content2":"SS插件大版本更新已经推送，更新后除了游戏模式\/V2,Pcap_DNSproxy,KCPTUN功能外，其余和arm机型一致。",
-"content3":"如果你用SS内置升级功能出现问题，建议卸载SS插件后重装，所有节点信息重新手动配置。",
-"content4":"如果还有问题，建议格式化jffs分区后重装SS插件，并手动配置节点信息。"
-}     
+"content2":"SS插件大版本更新已经推送，更新后除了游戏模式/V2、Pcap_DNSproxy、KCPTUN功能外，其余和arm机型一致。"
+"content3":"如果你用SS内置升级功能出现问题，建议卸载SS插件后重装，所有节点信息重新手动配置，配置完成后新生成一份备份。"
+"content4":"如果还有问题，建议格式化jffs分区后重装SS插件，并手动配置节点信息，如有问题，请到telegram群反馈。"
+}
