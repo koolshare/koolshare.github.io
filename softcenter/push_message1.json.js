78b69ab15600c00e6f2167b02b831842766f7277
lixin <sadoneli@gmail.com>
2017-07-14 10:22:39 +0800

    update message

--- softcenter/push_message1.json.js
+++ softcenter/push_message1.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>梅林改版固件&nbsp;-&nbsp;软件中心</em>",
-"content1":"<b><font size='4px'>你可能用了假的koolproxy！</font></b>",
-"content2":"因为我个人（sadog）的误操作，导致ss安装包里带了3.1.0-beta5的koolproxy程序，而更新ss插件会导致koolproxy程序会被这个老的koolproxy覆盖！",
-"content3":"这里建议大家更新koolproxy 3.2.5.1，并检查插件版本和运行状态里的版本是否一致（例如插件版本3.2.5.1和程序版本v3.2.5，则为一致），如果不一致，建议大家关闭重装koolproxy！",
-"content4":"这里对大家表示非常的抱歉！"
+"content1":"<b>通知（2017年7月14日 10:21:39）：</b>",
+"content2":"近日大家反馈的不能进入路由器界面的bug是由于之前更新的koolproxy导致的，现在已经修复！",
+"content3":"建议大家升级最新版本插件，或者卸载已有的koolproxy插件后重装（升级和重装操作均会保留证书）",
+"content4":"祝好！"
 }
