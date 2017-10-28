3b453906d2baf46c2d42c22e94329f57422895e0
sadoneli <sadoneli@gmail.com>
2017-10-29 01:14:37 +0800

    minor update

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -1,8 +1,8 @@
 {
 "version":"3.6.2",
-"md5":"7cb85a494e73bf6dd4f45305dc82b092",
+"md5":"8fd37946605e7973beb880a099049aec",
 "home_url":"Main_Ss_Content.asp",
 "title":"shadowsocks",
 "description":"科学上网",
-"build_date":"2017-10-28_23:20:00"
+"build_date":"2017-10-29_01:13:44"
 }
