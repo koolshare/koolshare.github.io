bc0011bb6e6e05a5d46b46f7a220cf2e25ec6af4
sadoneli <sadoneli@gmail.com>
2017-10-28 10:31:56 +0800

    update message

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>梅林改版固件&nbsp;-&nbsp;软件中心</em>",
-"content1":"<b>软件中心更新日志：（2017年10月25日）：</b>",
-"content2":"更新和优化了离线安装；对X7.7固件适配进行了调整；更新koolbox，新增base64、shuf支持",
-"content3":"",
+"content1":"<b>通知：</b>",
+"content2":"koolshare merlin改版固件X7.7已经发布。",
+"content3":"请前往<a href='http://koolshare.cn/forum-96-1.html' target='_blank'>论坛 - koolshare.cn</a>更新吧~",
 "content4":""
 }
