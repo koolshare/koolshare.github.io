c3a9c0c7d39fea594d109ae4cbad1765f5f12efb
sadoneli <sadoneli@gmail.com>
2017-12-12 23:05:27 +0800

    fix kcptun restart

--- shadowsocks/config.json.js
+++ shadowsocks/config.json.js
@@ -1,8 +1,8 @@
 {
-"version":"3.8.2",
-"md5":"6fbe0cb062e80f90aaade1545e1d995b",
+"version":"3.8.3",
+"md5":"ebc58e2429351cd00294ae69be9735ec",
 "home_url":"Main_Ss_Content.asp",
 "title":"科学上网",
 "description":"科学上网",
-"build_date":"2017-12-12_21:28:01"
+"build_date":"2017-12-12_23:05:13"
 }
