24cbfc2b09203f61fac2a566032861e3a2858d02
sadoneli <sadoneli@gmail.com>
2018-09-12 20:46:14 +0800

    update software center log

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,7 @@
 {
 "title":"<em>梅林改版固件&nbsp;-&nbsp;软件中心</em>",
 "content1":"<b>通知：</b>koolshare merlin改版固件X7.9已经发布，请前往论坛<a href='http://koolshare.cn/forum-96-1.html' target='_blank'> <em>http://koolshare.cn</em> </a>更新吧~",
-"content2":"1.3.4更新日志：修复离线安装的一些问题; 修复在网络情况差的情况下可能无法访问软件中心服务器的问题。",
+"content2":"1.4.0更新日志：修复离线安装的一些问题; 修复在网络情况差的情况下可能无法访问软件中心服务器的问题。",
 "content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
 <a href='https://github.com/koolshare/koolshare.github.io' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/xbchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
